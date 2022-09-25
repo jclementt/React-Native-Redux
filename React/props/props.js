8051e337c78d1f04067f353703228a1fe08529b3
jclement <joannapassos16@gmail.com>
2022-09-25 14:51:43 -0300

    React Native e Redux - udemy IX

---
+++ React/props/props.js
@@ -0,0 +1,40 @@
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      
+      this.state = { 
+        contador: 0,
+              nome: "Joanna Clementino"
+      };
+    }
+    
+    render() {
+      setTimeout(() => {
+        this.setState({
+          contador: this.state.contador + 1
+        })
+      }, 1000);
+      
+      return (
+        <div> 
+          <Contador valor={this.state.contador} />
+          <h2>{ this.state.nome }</h2>
+            </div>
+      )
+    }
+  }
+  
+  class Contador extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>{ this.props.valor }</h1>
+        </div>
+      )
+    }
+  }
+  
+  ReactDOM.render(
+        <App />,
+        document.getElementById('app')
+  );
